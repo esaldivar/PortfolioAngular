79cfba087304cde26cd73e56a902ffb908025cd6
Eric Saldivar <esaldivar1214@gmail.com>
2025-03-06 07:21:36 -0600

    feat(components): add About, Contact, Footer, Intro, Podcast, and Projects components with basic structure and templates

---
+++ src/app/podcast/podcast.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-podcast',
+  imports: [],
+  templateUrl: './podcast.component.html',
+  styleUrl: './podcast.component.scss'
+})
+export class PodcastComponent {
+
+}
