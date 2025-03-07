182b08ba5d4b2a0e564b82bfb2b49fb23f551ea0
Eric Saldivar <esaldivar1214@gmail.com>
2025-03-07 17:17:33 -0600

    feat(experience): add Experience component with basic structure and integrate into app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,26 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { LeftSideBarComponent } from "./left-side-bar/left-side-bar.component";
 import { RightSideBarComponent } from './right-side-bar/right-side-bar.component';
+import { IntroComponent } from './intro/intro.component';
+import { PodcastComponent } from "./podcast/podcast.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./footer/footer.component";
+import { ExperienceComponent } from "./experience/experience.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ HeaderComponent, LeftSideBarComponent, RightSideBarComponent],
+  imports: [
+    HeaderComponent,
+    LeftSideBarComponent,
+    RightSideBarComponent,
+    IntroComponent,
+    PodcastComponent,
+    AboutMeComponent,
+    ContactComponent, 
+    FooterComponent, 
+    ExperienceComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   host: {
