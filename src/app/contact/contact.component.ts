cc3b474e63d1ceb7cb7668032b022a5a6a9b45c1
Eric Saldivar <esaldivar1214@gmail.com>
2025-03-08 21:17:05 -0600

    feat(contact): implement reactive form for email submission with validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,30 +1,46 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../button/button.component";
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [ButtonComponent, FormsModule],
+  imports: [ButtonComponent, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
   displayForm= false;
-  emailDetails = {
-    name: '',
-    email: '',
-    message: ''
-  }
+
+  name = new FormControl('',[
+    Validators.required, Validators.minLength(2), Validators.pattern(/^[a-zA-Z\s]*$/)
+  ]);
+
+  email  = new FormControl('',[
+    Validators.required,
+    Validators.email
+  ]);
+
+  message = new FormControl('',[
+    Validators.required, Validators.minLength(10)
+  ]);
+
+  emailForm = new FormGroup({
+    email: this.email,
+    name: this.name,
+    message: this.message
+  });
 
   showEmailForm(e: Event) {
     e.preventDefault();
     this.displayForm = true;
   }
 
-  sendEmail() { 
-    this.displayForm = false;
-
+  sendEmail(e: Event) {
+    e.preventDefault(); 
+    if(this.emailForm.valid) {
+      this.displayForm = false;
+    }
   }
 
 }
