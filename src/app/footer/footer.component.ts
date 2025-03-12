f56be1acde3b78bd03206b4bf441e17b0b21a843
Eric Saldivar <esaldivar1214@gmail.com>
2025-03-11 21:10:35 -0500

    feat(footer): enhance footer with dynamic social links and improved styling

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SocialLink, Config } from '../../utils/config';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+socialLinks: SocialLink[] =  Config.socials;
 }
