55fad1d7e12cebd3e2ec2d3e48c5343ccfab1903
Eric Saldivar <esaldivar1214@gmail.com>
2025-03-11 20:14:11 -0500

    fix(contact): update sendEmail method to return success status correctly

--- src/app/contact/contact.service.ts
+++ src/app/contact/contact.service.ts
@@ -1,7 +1,6 @@
 import { FormControl, FormGroup } from '@angular/forms';
 import emailjs from 'emailjs-com';
 
-  
 interface ContactForm {
     name: FormControl<string | null>
     email: FormControl<string | null>
@@ -11,25 +10,22 @@ interface ContactForm {
     
     serviceID = import.meta.env.NG_APP_SERVICE_ID;
     templateID = import.meta.env.NG_APP_SERVICE_TEMPLATE;
-      
-    
-    sendEmail(form: FormGroup<ContactForm>): boolean {
 
+    sendEmail(form: FormGroup<ContactForm>): boolean {
         const templateParams = {
             from_name: form.get('name')?.value,
             email: form.get('email')?.value,
             message: form.get('message')?.value
           }
-        let emailSuccess = false;
         emailjs.send(this.serviceID, this.templateID, templateParams).then(
             (response) => {
               console.log('SUCCESS!', response.status, response.text);
-              emailSuccess = true;
+              return true;
             },
             (error) => {
               console.log('FAILED...', error);
             },
           );
-          return emailSuccess;
+          return false;
     }
   }
