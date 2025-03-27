df96bacc2b30f734ee81a62109683775a798f554
Eric Saldivar <esaldivar1214@gmail.com>
2025-03-27 08:44:36 -0500

    feat(work-experience): integrate job component into navigation with dynamic data display

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { JobComponent } from '../work-experience/job/job.component';
+import { Configuration, JobDetails } from '../../utils/config';
+import { CommonModule } from '@angular/common';
 
 @Component({
 	selector: 'work-nav',
-	imports: [NgbNavModule, NgbNavModule],
+	imports: [NgbNavModule, NgbNavModule, JobComponent, CommonModule],
 	templateUrl: './nav.component.html',
 })
 export class NgbdNavVertical {
 	active = 1;
+  workExperience: JobDetails[] = Configuration.workExperience;
 }
