b3d2b113387be175ee8f5cf14fa73707c3859c74
Eric Saldivar <esaldivar1214@gmail.com>
2025-04-01 11:57:54 -0500

    feat(projects): implement dynamic project layout with alternating alignment based on list number

--- src/app/projects/project-content/project-content.component.ts
+++ src/app/projects/project-content/project-content.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-project-content',
-  imports: [],
+  imports: [CommonModule ],
   templateUrl: './project-content.component.html',
   styleUrl: './project-content.component.scss'
 })
-export class ProjectContentComponent {
+export class ProjectContentComponent implements OnInit{
+  @Input({required:true}) listNumber: number = 0;
+  projectOnLeft: boolean = false;
 
+  ngOnInit(): void {
+    console.log('ProjectContentComponent');
+    this.listNumber % 2 === 0 ? this.projectOnLeft = true : this.projectOnLeft = false;
+  }
 }
