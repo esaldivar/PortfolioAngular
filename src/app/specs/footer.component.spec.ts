c763bf27eea6ed3802a1bdbdd6f83c2866d449d4
Eric Saldivar <esaldivar1214@gmail.com>
2025-04-01 20:53:03 -0500

    test(footer): add unit tests for FooterComponent and initialize socialLinks from Configuration

--- src/app/specs/footer.component.spec.ts
+++ src/app/specs/footer.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FooterComponent } from '../components/footer/footer.component';
+import { Configuration, SocialLink } from '../../utils/config';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -7,16 +8,28 @@ describe('FooterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FooterComponent]
-    })
-    .compileComponents();
+      imports: [FooterComponent], // Standalone component
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the footer component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize socialLinks with values from Configuration', () => {
+    expect(component.socialLinks).toEqual(Configuration.socials);
+  });
+
+  it('should render social links in the template', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const socialLinks = Configuration.socials;
+    socialLinks.forEach((link: SocialLink) => {
+      const anchor = compiled.querySelector(`a[href="${link.url}"]`);
+      expect(anchor).toBeTruthy();
+    });
   });
 });
