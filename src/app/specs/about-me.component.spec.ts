95c313f44ba6989cf764d4960ff4c878cbccd11e
Eric Saldivar <esaldivar1214@gmail.com>
2025-04-01 20:00:11 -0500

    test(about-me): enhance unit tests for AboutMeComponent with additional assertions

--- src/app/specs/about-me.component.spec.ts
+++ src/app/specs/about-me.component.spec.ts
@@ -1,22 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AboutMeComponent } from '../components/about-me/about-me.component';
+import { ElementRef } from '@angular/core';
 
-describe('AboutMeComponent', () => {
+describe('About Me Component', () => {
   let component: AboutMeComponent;
   let fixture: ComponentFixture<AboutMeComponent>;
+  let mockElementRef: ElementRef;
 
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
-      imports: [AboutMeComponent]
-    })
-    .compileComponents();
+      imports: [AboutMeComponent], // Standalone component
+      providers: []
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AboutMeComponent);
     component = fixture.componentInstance;
+
+    // Mock the @ViewChild property
+    mockElementRef = new ElementRef(document.createElement('section'));
+    component.about = mockElementRef;
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the about me component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize cdnUrl correctly', () => {
+    const expectedCdnUrl = import.meta.env.NG_APP_CLOUDFRONT_CDN + '/aboutme.webp';
+    expect(component.cdnUrl).toBe(expectedCdnUrl);
+  });
+
+   it('should set @ViewChild about element', () => {
+    expect(component.about).toBeDefined();
+    expect(component.about.nativeElement).toBeInstanceOf(HTMLElement);
   });
 });
